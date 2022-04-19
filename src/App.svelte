71a4f5ba157bd5ede1b170c98ccf00f8ce56a066
utx0 <utx0@mettadata.com>
2022-04-19 19:54:24 +1000

    101

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,64 @@
 <script>
-	export let name;
+    let firstName = "utx0";
+    let lastName = "_";
+    let color = 'blue'
+    let showText = false;
+    let users = [
+        {
+            id: '1',
+            name: 'John'
+        },
+        {
+            id: '2',
+            name: 'bob'
+        },
+        {
+            id: '3',
+            name: 'Lucy'
+        }
+    ]
+
+    $: name = firstName + '' + lastName;
+
+    const toggle = () => {
+        color = color === 'blue' ? 'red' : 'blue'
+        showText = !showText
+		users = [...users, { id: '4', name: 'Jan'}]
+    };
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <h1 style="color: {color}">Hello {name}!</h1>
+    {#if showText}
+        <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    {:else}
+        <p>No text</p>
+    {/if}
+    <button on:click={toggle}>Click</button>
+
+    {#each users as user (user.id)}
+		<h3>{user.id}: {user.name}</h3>
+    {/each}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
