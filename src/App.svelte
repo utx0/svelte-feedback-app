33c0396fa86e84265f3eb3b6462f7da8df0e9722
utx0 <utx0@mettadata.com>
2022-04-21 20:59:02 +1000

    new setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,64 +1,11 @@
 <script>
-    let firstName = "utx0";
-    let lastName = "_";
-    let color = 'blue'
-    let showText = false;
-    let users = [
-        {
-            id: '1',
-            name: 'John'
-        },
-        {
-            id: '2',
-            name: 'bob'
-        },
-        {
-            id: '3',
-            name: 'Lucy'
-        }
-    ]
 
-    $: name = firstName + '' + lastName;
-
-    const toggle = () => {
-        color = color === 'blue' ? 'red' : 'blue'
-        showText = !showText
-		users = [...users, { id: '4', name: 'Jan'}]
-    };
 </script>
 
 <main>
-    <h1 style="color: {color}">Hello {name}!</h1>
-    {#if showText}
-        <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-    {:else}
-        <p>No text</p>
-    {/if}
-    <button on:click={toggle}>Click</button>
-
-    {#each users as user (user.id)}
-		<h3>{user.id}: {user.name}</h3>
-    {/each}
+    <h1>Hello</h1>
 </main>
 
 <style>
-    main {
-        text-align: center;
-        padding: 1em;
-        max-width: 240px;
-        margin: 0 auto;
-    }
 
-    h1 {
-        color: #ff3e00;
-        text-transform: uppercase;
-        font-size: 4em;
-        font-weight: 100;
-    }
-
-    @media (min-width: 640px) {
-        main {
-            max-width: none;
-        }
-    }
 </style>
