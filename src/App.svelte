df438d584853547909832e360000ae1cc6c79c05
utx0 <utx0@mettadata.com>
2022-04-22 13:42:35 +1000

    reviews, ratings, avg

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,40 @@
 <script>
+    import FeedbackList from "./components/FeedbackList.svelte";
+    import FeedbackStats from "./components/FeedbackStats.svelte";
 
+    let feedback = [
+        {
+            id: 1,
+            rating: 10,
+            text: "To the crushed walnut add herring, leek, orange juice and puréed chicory."
+        },
+        {
+            id: 2,
+            rating: 9,
+            text: "Mix each side of the pork butt with a dozen pounds of pork shoulder."
+        },
+        {
+            id:3,
+            rating: 2,
+            text: "Varnish a dozen pounds of chili in a handfull and a half teaspoons of condensed milk."
+        },
+        {
+            id:4,
+            rating: 7,
+            text:"Remember: roasted sauerkraut tastes best when drained in a casserole brushed with baking powder."
+        }
+    ]
+
+    $: count = feedback.length
+    $: avg = feedback.reduce((avg, {rating}) => avg + rating, 0) / feedback.length
+
+    const deleteFeedback = (e) => {
+        const itemId = e.detail
+        feedback = feedback.filter((item) => item.id != itemId)
+    }
 </script>
 
-<main>
-    <h1>Hello</h1>
+<main class="container">
+    <FeedbackStats {count} {avg} />
+    <FeedbackList {feedback} on:delete-feedback={deleteFeedback}/>
 </main>
-
-<style>
-
-</style>
---
+++ src/components/FeedbackStats.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let count
+    export let avg
+</script>
+
+<div class="feedback-stats">
+    <h4>{count} Reviews</h4>
+    <h4>Ratings Avg: {avg}</h4>
+</div>
+
+<style>
+    .feedback-stats {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+</style>
